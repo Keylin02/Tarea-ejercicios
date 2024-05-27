fa0dc88ccf1e241e9417fdf974da5f5074692dbe
keylin02 <keydalorso@gmail.com>
2024-05-27 00:06:53 -0600

    tarea de ejercicios de programacion 2

---
+++ Tarea_aumento_salariales/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_aumento_salariales
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int operarios = 0, tecnicos = 0, profesionales = 0, tipoEmpleado = 0;
+            double salarioNetoOperarios = 0, salarioNetoTecnicos = 0, salarioNetoProfesionales = 0, precioHora = 0, horasLaboradas = 0, salarioOrdinario = 0, aumento = 0, salarioBruto = 0, deduccionCCSS = 0, salarioNeto = 0;
+            string cedula, nombre, otroEmpleado;
+
+            while (true)
+            {
+                Console.WriteLine("Ingrese datos del empleado");
+                Console.WriteLine("Número de cédula:");
+                cedula = Console.ReadLine();
+                Console.WriteLine("Nombre:");
+                nombre = Console.ReadLine();
+                Console.WriteLine("Tipo de empleado: 1) Operario, 2) Técnico, 3) Profesional");
+                tipoEmpleado = int.Parse(Console.ReadLine());
+                Console.WriteLine("Precio por hora:");
+                precioHora = double.Parse(Console.ReadLine());
+                Console.WriteLine("Cantidad de horas laboradas:");
+                horasLaboradas = double.Parse(Console.ReadLine());
+
+                salarioOrdinario = precioHora * horasLaboradas;
+
+                switch (tipoEmpleado)
+                {
+                    case 1:
+                        aumento = salarioOrdinario * 0.15;
+                        operarios++;
+                        salarioNetoOperarios = salarioOrdinario + aumento;
+                        break;
+                    case 2:
+                        aumento = salarioOrdinario * 0.10;
+                        tecnicos++;
+                        salarioNetoTecnicos = salarioOrdinario + aumento;
+                        break;
+                    case 3:
+                        aumento = salarioOrdinario * 0.05;
+                        profesionales++;
+                        salarioNetoProfesionales = salarioOrdinario + aumento;
+                        break;
+
+                }
+
+                salarioBruto = salarioOrdinario + aumento;
+                deduccionCCSS = salarioBruto * 0.0917;
+                salarioNeto = salarioBruto - deduccionCCSS;
+
+                Console.WriteLine("Datos del empleado");
+                Console.WriteLine("Cédula: " + cedula);
+                Console.WriteLine("Nombre: " + nombre);
+                Console.WriteLine("Tipo de empleado: " + tipoEmpleado);
+                Console.WriteLine("Salario por hora: " + precioHora);
+                Console.WriteLine("Cantidad de horas: " + horasLaboradas);
+                Console.WriteLine("Salario ordinario: " + salarioOrdinario);
+                Console.WriteLine("Aumento: " + aumento);
+                Console.WriteLine("Salario bruto: " + salarioBruto);
+                Console.WriteLine("DeducciónCCSS: " + deduccionCCSS);
+                Console.WriteLine("Salario neto: " + salarioNeto);
+
+                Console.WriteLine("Desea ingresar otro empleado? si desea marque S por el contrario marque N");
+                otroEmpleado = Console.ReadLine();
+                if (otroEmpleado != "S")
+                    break;
+
+            }
+            Console.WriteLine("1: Cantidad empleados tipo operarios: " + operarios);
+            Console.WriteLine("2: Acomulado salario neto para operarios: " + salarioNetoOperarios);
+            Console.WriteLine("3: Promedio salario neto para operarios: " + salarioNetoOperarios / operarios);
+            Console.WriteLine("4: Cantidad empleado tipo técnico: " + tecnicos);
+            Console.WriteLine("5: Acumulado salario neto para técnicos: " + salarioNetoTecnicos);
+            Console.WriteLine("6: Promedio salario neto para técnicos: " + salarioNetoTecnicos / tecnicos);
+            Console.WriteLine("7: Cantidad empleados tipo profesional: " + profesionales);
+            Console.WriteLine("8: Acumulado salario neto para profesional: " + salarioNetoProfesionales);
+            Console.WriteLine("9: Promedio salario neto para profesional: " + salarioNetoProfesionales / profesionales);
+            
+
+            Console.ReadKey();
+        } 
+        
+    }
+}
---
+++ tarea_futbol/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tarea_futbol
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int entradasSol = 0, entradasSombra = 0, entradasPreferencial = 0, numFactura = 0, localidad = 0, cantidadEntradas = 0;
+            double acumuladoSol = 0, acumuladosSombra = 0, acumuladosPreferencial = 0, precioEntrada = 0, subtotal = 0, cargos = 0, totalPagar = 0;
+            string cedula, nombre, nombreLocalizacion, otraVenta;
+
+            while (true)
+            {
+                Console.WriteLine("Ingrese datos de la venta");
+                Console.WriteLine("Número de factura");
+                numFactura = int.Parse(Console.ReadLine());
+                Console.WriteLine("Cédula:");
+                cedula = Console.ReadLine();
+                Console.WriteLine("Nombre comprador:");
+                nombre = Console.ReadLine();
+                Console.WriteLine("Localización");
+                Console.WriteLine("1: Sol Norte/Sur");
+                Console.WriteLine("2: Sombre Este/Oeste");
+                Console.WriteLine("3: Preferencial");
+                localidad = int.Parse(Console.ReadLine());
+                Console.WriteLine("Cantidad de entradas:");
+                cantidadEntradas = int.Parse(Console.ReadLine());
+
+                if (cantidadEntradas > 4)
+                {
+                    Console.WriteLine("Limite de entradas, solo 4 por persona");
+                    continue;
+                }
+                              
+
+                switch (localidad)
+                {
+                    case 1:
+                        precioEntrada = 10500;
+                        nombreLocalizacion = "Sol Norte/Sur";
+                        entradasSol = cantidadEntradas;
+                        acumuladoSol = precioEntrada * cantidadEntradas;
+                        break;
+                    case 2:
+                        precioEntrada = 20500;
+                        nombreLocalizacion = "sombra Este/Oeste";
+                        entradasSombra = cantidadEntradas;
+                        acumuladosSombra = precioEntrada * cantidadEntradas;
+                        break;
+                    case 3:
+                        precioEntrada = 25500;
+                        nombreLocalizacion = "Preferencial";
+                        entradasPreferencial = cantidadEntradas;
+                        acumuladosPreferencial = precioEntrada * cantidadEntradas;
+                        break;
+                }
+
+                subtotal = precioEntrada * cantidadEntradas;
+                cargos = 1000 * cantidadEntradas;
+                totalPagar = subtotal + cargos;
+
+                Console.WriteLine("Datos de la factura");
+                Console.WriteLine("Número de factura: " + numFactura);
+                Console.WriteLine("Cédula: " + cedula);
+                Console.WriteLine("Nombre comprador:" + nombre);
+                Console.WriteLine("Localidad: " + localidad);
+                Console.WriteLine("Cantidad entradas: " + cantidadEntradas);
+                Console.WriteLine("Subtotal: " + subtotal);
+                Console.WriteLine("Cargos por servicios: " + cargos);
+                Console.WriteLine("Total a pagar: " + totalPagar);
+
+                Console.WriteLine("Desea ingresar otra venta? si desea marque S si no N");
+                otraVenta = Console.ReadLine();
+
+                if (otraVenta != "S")                
+                    break;  
+
+            }
+            Console.WriteLine("Cantidad entradas localidad sol Norte/Sur: " + entradasSol);
+            Console.WriteLine("Acumulado dinero localidad sol Norte/Sur: " + acumuladoSol);
+            Console.WriteLine("Cantidad entradas localidad sombra Este/Oeste: " + entradasSombra);
+            Console.WriteLine("Acumulado dinero localidad sombra Este/Oeste: " + acumuladosSombra);
+            Console.WriteLine("Cantidad entradas localidad preferencial: " + entradasPreferencial);
+            Console.WriteLine("Acumulado dinero localidad preferencial: " + acumuladosPreferencial);
+
+            Console.ReadKey();
+
+        }     
+                
+    }
+}
